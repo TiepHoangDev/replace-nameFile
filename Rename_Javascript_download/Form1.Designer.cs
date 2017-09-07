c34be8983b952edfd06f622665466f4551af89f1
tiephoangdev <tiephoang.dev@gmail.com>
2017-09-06 17:00:40 -0700

    001

---
+++ Rename_Javascript_download/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Rename_Javascript_download
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtNewName = new System.Windows.Forms.TextBox();
+            this.txtOldName = new System.Windows.Forms.TextBox();
+            this.txtForder = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lvFolder = new System.Windows.Forms.ListView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblRun = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.btnReload);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblRun);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnRun);
+            this.panel1.Controls.Add(this.txtNewName);
+            this.panel1.Controls.Add(this.txtOldName);
+            this.panel1.Controls.Add(this.txtForder);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(662, 112);
+            this.panel1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(184, 69);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(267, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(390, 41);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(61, 23);
+            this.btnReload.TabIndex = 6;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Thay tên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Fordel";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(103, 69);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Rename";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtNewName
+            // 
+            this.txtNewName.Location = new System.Drawing.Point(280, 16);
+            this.txtNewName.Name = "txtNewName";
+            this.txtNewName.Size = new System.Drawing.Size(171, 20);
+            this.txtNewName.TabIndex = 3;
+            // 
+            // txtOldName
+            // 
+            this.txtOldName.Location = new System.Drawing.Point(103, 15);
+            this.txtOldName.Name = "txtOldName";
+            this.txtOldName.Size = new System.Drawing.Size(171, 20);
+            this.txtOldName.TabIndex = 3;
+            this.txtOldName.Text = ".tải xuống";
+            // 
+            // txtForder
+            // 
+            this.txtForder.Location = new System.Drawing.Point(103, 43);
+            this.txtForder.Name = "txtForder";
+            this.txtForder.Size = new System.Drawing.Size(281, 20);
+            this.txtForder.TabIndex = 3;
+            this.txtForder.Click += new System.EventHandler(this.txtForder_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lvFolder);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 112);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(662, 259);
+            this.panel2.TabIndex = 4;
+            // 
+            // lvFolder
+            // 
+            this.lvFolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvFolder.Location = new System.Drawing.Point(0, 0);
+            this.lvFolder.Name = "lvFolder";
+            this.lvFolder.Size = new System.Drawing.Size(662, 259);
+            this.lvFolder.TabIndex = 3;
+            this.lvFolder.UseCompatibleStateImageBehavior = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // lblRun
+            // 
+            this.lblRun.AutoSize = true;
+            this.lblRun.Location = new System.Drawing.Point(457, 74);
+            this.lblRun.Name = "lblRun";
+            this.lblRun.Size = new System.Drawing.Size(27, 13);
+            this.lblRun.TabIndex = 5;
+            this.lblRun.Text = "Run";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 371);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView lvFolder;
+        private System.Windows.Forms.TextBox txtNewName;
+        private System.Windows.Forms.TextBox txtOldName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtForder;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblRun;
+
+    }
+}
+
---
+++ Rename_Javascript_download/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rename_Javascript_download
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lvFolder.View = View.List;
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void txtForder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtForder.Text = folderBrowserDialog1.SelectedPath;
+                showFile(txtForder.Text);
+            }
+        }
+
+        private void showFile(string path)
+        {
+            lvFolder.Clear();
+            var allFile = Directory.GetFiles(path);
+            if (allFile.Length == 0)
+            {
+                MessageBox.Show("Folder ko có file");
+                return;
+            }
+            foreach (var item in allFile)
+            {
+                if (item.Contains(txtOldName.Text))
+                {
+                    lvFolder.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtOldName.Text))
+            {
+                MessageBox.Show("Nhập kí tự cũ cần thay");
+                return;
+            }
+            if (MessageBox.Show("Sure? đổi tên?", "Sure", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning) != System.Windows.Forms.DialogResult.Yes) return;
+            progressBar1.Value = 0;
+            progressBar1.Maximum = lvFolder.Items.Count - 1;
+            progressBar1.Step = 1;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        void showRun(int value)
+        {
+            lblRun.Text = "oke " + value + "/" + lvFolder.Items.Count;
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            showFile(txtForder.Text);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < lvFolder.Items.Count; i++)
+            {
+                var item = lvFolder.Items[i];
+                try
+                {
+                    File.Move(item.Text, item.Text.Replace(txtOldName.Text, txtNewName.Text));
+                }
+                catch (IOException)
+                {
+                    File.Delete(item.Text);
+                }
+                backgroundWorker1.ReportProgress(i);
+                showRun(i + 1);
+            }
+            showFile(txtForder.Text);
+            }
+            catch (Exception exx)
+            {
+                MessageBox.Show(exx.Message);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Done");
+        }
+    }
+}
